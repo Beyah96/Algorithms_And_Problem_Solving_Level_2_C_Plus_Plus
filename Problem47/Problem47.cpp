cd54a338d411ac04ddcebcb5cf0c1343069dcb2f
Beyah96 <bahsaid96@gmail.com>
2024-09-22 16:03:41 +0200

    Added solution for Problem 47: Rounding a Floting-Point Number

---
+++ Problem47/Problem47.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadNumber() {
+	float Number;
+	cout << "Please enter a Number : ";
+	cin >> Number;
+	return Number;
+}
+
+float MyABS(float Number) {
+	if (Number < 0)
+		Number *= -1;
+	return Number;
+}
+
+int MyRound(float Number) {
+	if ((int)abs((Number * 10)) % 10 >= 5)
+		if (Number > 0)
+			Number += 1;
+		else
+			Number = Number-1;
+	return int(Number);
+}
+
+
+int main() {
+	float Number = ReadNumber();
+	cout << "My Round : " << MyRound(Number) << endl;
+	cout << "C++ Round : " << round(Number);
+}
