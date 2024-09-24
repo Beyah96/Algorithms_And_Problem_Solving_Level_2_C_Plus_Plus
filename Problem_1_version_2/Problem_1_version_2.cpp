773f34acc2d8398c26b0ceb36e9c222f26a6f0cb
Beyah96 <bahsaid96@gmail.com>
2024-09-24 23:42:16 +0200

    Added CheckWinner function to determine round winner based on player and computer choices. Updates game results and displays the round outcome.

--- Problem_1_version_2/Problem_1_version_2.cpp
+++ Problem_1_version_2/Problem_1_version_2.cpp
@@ -19,3 +19,107 @@ struct stGameResult {
 };
 
 
+int ReadNumber(string Message, int Limit) {
+	int Number;
+	do {
+		cout << Message;
+		cin >> Number;
+	} while (Number < 0 || Number > Limit);
+	return Number;
+}
+
+int RandomNumber(int From, int To) {
+	return rand() % (To - From + 1) + From;
+}
+
+enChoice PlayerChoiceFunction() {
+	return (enChoice)ReadNumber("Your Choice [1] : Stone, [2] : Paper, [3] : Scissor ? : ", 4);
+}
+
+enChoice ComputerChoiceFunction() {
+	return (enChoice)RandomNumber(1, 3);
+}
+
+void ScreenColor(enWinner Winner) {
+	string Message;
+	if (Winner == enWinner::Computer)
+		Message = "color " + to_string(int(enWinner::Computer)) + "F";
+	else if (Winner == enWinner::Player)
+		Message = "color " + to_string(int(enWinner::Player)) + "F";
+	else
+		Message = "color " + to_string(int(enWinner::NoWinner)) + "F";
+
+	system(Message.c_str());
+}
+
+string CheckFinalWinner(stGameResult Game) {
+	if (Game.PlayerWonCount > Game.ComputerWonCount)
+		return "Player";
+	else if (Game.ComputerWonCount > Game.PlayerWonCount)
+		return "Computer";
+	else
+		return "No Winner";
+}
+
+string PrintChoice(enChoice Choice) {
+	switch (Choice)
+	{
+	case enChoice::Stone:
+		return "Stone";
+	case enChoice::Paper:
+		return "Paper";
+	case enChoice::Scissor:
+		return "Scissor";
+	default:
+		return "Unkown choice";
+	}
+}
+
+string RoundWinnerName(enWinner Winner) {
+	switch (Winner) {
+	case enWinner::NoWinner:
+		return "No Winner";
+	case enWinner::Computer:
+		return "Computer";
+	case enWinner::Player:
+		return "Player";
+	default:
+		return "Unkwon winner";
+	}
+}
+
+void PrintRound(stRoundInfo Round) {
+	cout << "\n\n------------ Round " + to_string(Round.RoundNumber) + " ------------ \n";
+	cout << "Player   Choice : " << PrintChoice(Round.PlayerChoice) << endl;
+	cout << "Computer Choice : " << PrintChoice(Round.ComputerChoice) << endl;
+	cout << "Round   Winner  : " << RoundWinnerName(Round.Winner) << endl;
+	cout << "--------------------------------- \n\n";
+}
+
+void CheckWinner(stGameResult& GameResult, stRoundInfo& RoundInfo) {
+	RoundInfo.PlayerChoice = PlayerChoiceFunction();
+	RoundInfo.ComputerChoice = ComputerChoiceFunction();
+	if (RoundInfo.PlayerChoice == RoundInfo.ComputerChoice) {
+		GameResult.DrawCount++;
+		RoundInfo.Winner = enWinner::NoWinner;
+	}
+	else if ((RoundInfo.PlayerChoice == enChoice::Stone && RoundInfo.ComputerChoice == enChoice::Scissor) ||
+		(RoundInfo.PlayerChoice == enChoice::Paper && RoundInfo.ComputerChoice == enChoice::Stone) ||
+		(RoundInfo.PlayerChoice == enChoice::Scissor && RoundInfo.ComputerChoice == enChoice::Paper)) {
+		GameResult.PlayerWonCount++;
+		RoundInfo.Winner = enWinner::Player;
+	}
+	else {
+		GameResult.ComputerWonCount++;
+		RoundInfo.Winner = enWinner::Computer;
+		cout << "\a" << endl;
+	}
+	ScreenColor(RoundInfo.Winner);
+	PrintRound(RoundInfo);
+}
+
+
+
+
+
+
