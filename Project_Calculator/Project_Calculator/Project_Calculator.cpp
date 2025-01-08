ac61067f5ecd94eda8edca8ed903678823de1718
beyah96 <bahsaid96@gmail.com>
2025-01-08 20:35:33 +0100

    Adding a new simple project Project_Calculator

--- Project_Calculator/Project_Calculator/Project_Calculator.cpp
+++ Project_Calculator/Project_Calculator/Project_Calculator.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 
 using namespace std;
-
+static short GameScore;
 
 enum enLevel {Easy = 1, Med = 2, Hard = 3, Mix = 4 };
-enum enOperationType { Add = 1, Sub = 2, Mul = 3, Div = 4 };
+enum enOperationType { Add = 1, Sub = 2, Mul = 3, Div = 4, Mix = 5 };
 struct stQuiz {
 	short Number;
 	enLevel Level;
@@ -37,30 +37,83 @@ enOperationType AskUserForOperationType() {
 }
 
 
-void GetOperationMembers() {
-
+vector <short> GetOperationMembers() {
+	enLevel Level = AskUserForLevel();
+	switch (Level)
+	{
+	case Easy:
+		return { GetRandomNumber(0, 10), GetRandomNumber(0, 10) };
+	case Med:
+		return { GetRandomNumber(10, 100), GetRandomNumber(10, 100) };
+	case Hard:
+		return { GetRandomNumber(100, 1000), GetRandomNumber(100, 1000) };
+	case Mix:
+		return { GetRandomNumber(0, 1000), GetRandomNumber(0, 1000) };
+	}
 }
 
-short Score(short Number) {
-	return Number;
+enOperationType PickRandomOperation(vector <enOperationType> OperationsType) {
+	return OperationsType[GetRandomNumber(0, OperationsType.size())];
+}
+
+vector <enOperationType> GetOperationsType(enOperationType OperationType) {
+	switch (OperationType)
+	{
+	case Add:
+		return { enOperationType::Add };
+	case Sub:
+		return { enOperationType::Sub };
+	case Mul:
+		return { enOperationType::Mul };
+	case Div:
+		return { enOperationType::Div };
+	case Mix:
+		return GetOperationsType(PickRandomOperation({OperationType}));
+	}
+}	
+
+int GetOperationResult(vector <short> vMembers, enOperationType PickedOpeation) {
+	switch (PickedOpeation)
+	{
+	case Add:
+		return vMembers[0] + vMembers[1];
+	case Sub:
+		return vMembers[0] - vMembers[1];
+	case Mul:
+		return vMembers[0] * vMembers[1];
+	case Div:
+		return vMembers[0] / vMembers[1];
+	case Mix:
+		GetOperationResult(vMembers, PickedOpeation);
+	}
+}
+
+enum enResult {Correct  = 1, Wrong = 2};
+
+bool isResultCorrect(enResult Result) {
+	return (Result == enResult::Correct) ? true : false;
+}
+short Score(enResult Result) {
+	return  isResultCorrect(Result) ? GameScore++ : GameScore ;
 }
 
 
 
 void PlayGame() {
-	short Number = AskUserForNumber("How many questions do you want to answer? : ", 0, 10);
-	enLevel Level = AskUserForLevel();
+	short Number = AskUserForNumber("How many questions do you want to answer? : ", 0, 10); // Number of Rounds
+	
 	enOperationType OperationType = AskUserForOperationType();
 
+	//cout << "Result : " << GetOperationResult(GetOperationMembers(), );
 	for (int i = 0; i < Number; i++) {
 		cout << "Question [" << i + 1 << "/" << Number <<"] :" << endl;
-		cout << "Score : " << Score(i) << endl;
+		//cout << "Score : " << Score(i) << endl;
 	}
 }
 int main() {
 	srand((unsigned)time(NULL));
 	//PlayGame();
-	cout << GetRandomNumber(37, 39);
+	
 	system("pause>0");
 	return 0;
 }
