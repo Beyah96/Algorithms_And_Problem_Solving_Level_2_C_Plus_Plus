36793ece547152e3ce2166d5a69e70665ce6847b
beyah96 <bahsaid96@gmail.com>
2025-01-09 23:58:16 +0100

    Project Calculator

--- Project_Calculator/Project_Calculator/Project_Calculator.cpp
+++ Project_Calculator/Project_Calculator/Project_Calculator.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 static short GameScore;
 
-enum enLevel {Easy = 1, Med = 2, Hard = 3, Mix = 4 };
-enum enOperationType { Add = 1, Sub = 2, Mul = 3, Div = 4, Mix = 5 };
+enum enLevel {Easy = 1, Med = 2, Hard = 3, MixLevels = 4 };
+enum enOperationType { Add = 1, Sub = 2, Mul = 3, Div = 4, MixOperations = 5 };
 struct stQuiz {
 	short Number;
 	enLevel Level;
@@ -37,8 +37,8 @@ enOperationType AskUserForOperationType() {
 }
 
 
-vector <short> GetOperationMembers() {
-	enLevel Level = AskUserForLevel();
+vector <short> GetOperationMembers(enLevel Level) {
+	
 	switch (Level)
 	{
 	case Easy:
@@ -47,7 +47,7 @@ vector <short> GetOperationMembers() {
 		return { GetRandomNumber(10, 100), GetRandomNumber(10, 100) };
 	case Hard:
 		return { GetRandomNumber(100, 1000), GetRandomNumber(100, 1000) };
-	case Mix:
+	case MixLevels:
 		return { GetRandomNumber(0, 1000), GetRandomNumber(0, 1000) };
 	}
 }
@@ -67,8 +67,8 @@ vector <enOperationType> GetOperationsType(enOperationType OperationType) {
 		return { enOperationType::Mul };
 	case Div:
 		return { enOperationType::Div };
-	case Mix:
-		return GetOperationsType(PickRandomOperation({OperationType}));
+	case MixOperations:
+		return { GetOperationsType(PickRandomOperation({OperationType})) };
 	}
 }	
 
@@ -83,7 +83,7 @@ int GetOperationResult(vector <short> vMembers, enOperationType PickedOpeation) 
 		return vMembers[0] * vMembers[1];
 	case Div:
 		return vMembers[0] / vMembers[1];
-	case Mix:
+	case MixOperations:
 		GetOperationResult(vMembers, PickedOpeation);
 	}
 }
@@ -101,19 +101,20 @@ short Score(enResult Result) {
 
 void PlayGame() {
 	short Number = AskUserForNumber("How many questions do you want to answer? : ", 0, 10); // Number of Rounds
-	
+	enLevel Level = AskUserForLevel();
+
 	enOperationType OperationType = AskUserForOperationType();
 
 	//cout << "Result : " << GetOperationResult(GetOperationMembers(), );
 	for (int i = 0; i < Number; i++) {
 		cout << "Question [" << i + 1 << "/" << Number <<"] :" << endl;
-		//cout << "Score : " << Score(i) << endl;
+		GetOperationMembers();
 	}
 }
 int main() {
 	srand((unsigned)time(NULL));
-	//PlayGame();
-	
+	PlayGame();
+
 	system("pause>0");
 	return 0;
 }
