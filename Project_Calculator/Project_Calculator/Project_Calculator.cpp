6c23733897693dffa87be55824aaaa5a72c5aff4
beyah96 <bahsaid96@gmail.com>
2025-01-11 20:28:10 +0100

    Refonte of code

--- Project_Calculator/Project_Calculator/Project_Calculator.cpp
+++ Project_Calculator/Project_Calculator/Project_Calculator.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <string>
 #include <cstdlib>
-#include <vector>
-
 using namespace std;
-static short GameScore;
 
-enum enLevel {Easy = 1, Med = 2, Hard = 3, MixLevels = 4 };
+
+enum enLevel { Easy = 1, Med = 2, Hard = 3, MixLevels = 4 };
 enum enOperationType { Add = 1, Sub = 2, Mul = 3, Div = 4, MixOperations = 5 };
-struct stQuiz {
-	short Number;
+struct stQuestion {
+	short Number1 = 0, Number2 = 0;
 	enLevel Level;
 	enOperationType OperationType;
+	int CorrectAnswer, PlayerAnswer;
+	bool AnswerResult = false;
 };
+
+struct stQuiz {
+	stQuestion QuestionsList[100];
+	short NumberOfQuestions;
+	short NumberOfWrongAnswers = 0;
+	bool isPass = false;
+};
+
 
 short AskUserForNumber(string Message, short minNumber, short maxNumber) {
 	short Number;
@@ -29,107 +36,99 @@ short GetRandomNumber(short From, short To) {
 
 
 enLevel AskUserForLevel() {
-	return (enLevel) AskUserForNumber("Enter question level [1] Easy, [2] Med, [3] Hard, [4] Mix : ", 1, 4);
+	return (enLevel)AskUserForNumber("Enter question level [1] Easy, [2] Med, [3] Hard, [4] Mix : ", 1, 4);
 }
 
 enOperationType AskUserForOperationType() {
-	return (enOperationType) AskUserForNumber("Enter operation type [1] Add, [2] Sub, [3] Mul, [4] Div, [5] Mix : ", 1, 4);
+	return (enOperationType)AskUserForNumber("Enter operation type [1] Add, [2] Sub, [3] Mul, [4] Div, [5] Mix : ", 1, 4);
 }
 
 
-vector <short> GetOperationMembers(enLevel Level) {
-	
-	switch (Level)
+void ResetScreen() {
+	system("cls");
+	system("color 0F");
+};
+
+enLevel GetRandomLevel() {
+	return (enLevel)GetRandomNumber(1, 3);
+}
+
+enOperationType GetRandomOperation() {
+	return (enOperationType)GetRandomNumber(1, 4);
+}
+
+int SimpleCalculator(short Number1, short Number2, enOperationType Operation) {
+	switch (Operation)
+	{
+	case Add:
+		return Number1 + Number2;
+	case Sub:
+		return Number1 - Number2;
+	case Mul:
+		return Number1 * Number2;
+	case Div:
+		return Number1 / Number2;
+	default:
+		return Number1 + Number2;
+	}
+}
+stQuestion GenerateQuestion(stQuiz Quiz) {
+	stQuestion Question;
+
+	if (Quiz.QuestionsList->Level == enLevel::MixLevels)
+		Question.Level = GetRandomLevel();
+	if (Quiz.QuestionsList->OperationType == enOperationType::MixOperations)
+		Question.OperationType = GetRandomOperation();
+
+	switch (Question.OperationType)
 	{
 	case Easy:
-		return { GetRandomNumber(0, 10), GetRandomNumber(0, 10) };
+		Question.Number1 = GetRandomNumber(1, 10);
+		Question.Number2 = GetRandomNumber(1, 10);
 	case Med:
-		return { GetRandomNumber(10, 100), GetRandomNumber(10, 100) };
+		Question.Number1 = GetRandomNumber(10, 100);
+		Question.Number2 = GetRandomNumber(10, 100);
 	case Hard:
-		return { GetRandomNumber(100, 1000), GetRandomNumber(100, 1000) };
-	case MixLevels:
-		return { GetRandomNumber(0, 1000), GetRandomNumber(0, 1000) };
+		Question.Number1 = GetRandomNumber(100, 1000);
+		Question.Number2 = GetRandomNumber(100, 1000);
+	default:
+		break;
 	}
+	Question.CorrectAnswer = SimpleCalculator(Question.Number1, Question.Number2, Question.OperationType);
+	return Question;
+}
+void GenerateQuizQuestions(stQuiz Quiz) {
+	for (short Question = 0; Question < Quiz.NumberOfQuestions; Question++)
+		Quiz.QuestionsList[Question] = GenerateQuestion(Quiz);
+};
+void AskAndCorrectQuestionsListAnswers(stQuiz Quiz) {
+};
+void PrintQuizResult(stQuiz Quiz) {};
+
+void PlayMathGame() {
+	stQuiz Quiz;
+	Quiz.NumberOfQuestions = AskUserForNumber("How many questions do you want to answer? : ", 0, 10); // Number of Rounds
+	Quiz.QuestionsList->Level = AskUserForLevel();
+	Quiz.QuestionsList->OperationType = AskUserForOperationType();
+
+	GenerateQuizQuestions(Quiz);
+	AskAndCorrectQuestionsListAnswers(Quiz);
+	PrintQuizResult(Quiz);
+
+};
+
+void StartGame() {
+	char PlayNewGame = 'Y';
+	do {
+		ResetScreen();
+		PlayMathGame();
+		cout << "Do you want to play a new game ? : ";
+		cin >> PlayNewGame;
+	} while (toupper(PlayNewGame) == 'Y');
 }
 
-enOperationType PickRandomOperation() {
-	return (enOperationType) GetRandomNumber(1,6);
-}
-
-enOperationType GetOperationsType(enOperationType OperationType) {
-	if (OperationType == enOperationType::MixOperations)
-		return GetOperationsType(PickRandomOperation());
-	else
-		return OperationType;
-}	
-
-int GetOperationResult(vector <short> vMembers, enOperationType PickedOpeation) {
-	switch (PickedOpeation)
-	{
-	case Add:
-		return vMembers[0] + vMembers[1];
-	case Sub:
-		return vMembers[0] - vMembers[1];
-	case Mul:
-		return vMembers[0] * vMembers[1];
-	case Div:
-		return vMembers[0] / vMembers[1];
-	case MixOperations:
-		GetOperationResult(vMembers, PickedOpeation);
-	}
-}
-
-
-bool isResultCorrect(int Result, int userResult) {
-	return (Result == userResult) ? true : false;
-}
-short Score(int Result, int userResult) {
-	return  isResultCorrect(Result, userResult) ? GameScore++ : GameScore ;
-}
-
-char PrintOperationSign(enOperationType OperationType) {
-	switch (OperationType)
-	{
-	case Add:
-		return '+';
-	case Sub:
-		return '-';
-	case Mul:
-		return '*';
-	case Div:
-		return '/';
-	}
-}
-
-void PlayGame() {
-	short Number = AskUserForNumber("How many questions do you want to answer? : ", 0, 10); // Number of Rounds
-	enLevel Level = AskUserForLevel();
-	vector <short> vMembers;
-	enOperationType OperationType = AskUserForOperationType();
-	
-	vector <enOperationType> OperationsType;
-	int Result, userInput;
-	for (int i = 0; i < Number; i++)
-		OperationsType.push_back((GetOperationsType(OperationType)));
-
-	for (int i = 0; i < Number; i++) {
-		cout << endl << endl << "Question [" << i + 1 << "/" << Number <<"] :" << endl;
-		vMembers = GetOperationMembers(Level);
-		Result = GetOperationResult(vMembers, OperationsType[i]);
-		cout << vMembers[0] << endl << PrintOperationSign(OperationsType[i]) << endl << vMembers[1] << endl << "___________" << endl;
-		cin >> userInput;
-		cout << "The result : " << Result << endl;
-		cout << (isResultCorrect(Result, userInput)) ? "Correct" : "Wrong";
-		system("color 2F");
-		Score(Result, userInput);
-		cout << endl << "Score : " << GameScore;
-		vMembers.clear();
-		
-	}
-}
 int main() {
 	srand((unsigned)time(NULL));
-	PlayGame();
 
 	system("pause>0");
 	return 0;
